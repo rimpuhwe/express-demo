0a7c0b330495809c07c5a5b2a502e5651500ae29
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-28 13:51:40 +0200

    setting the environmet file and deal with some route methods

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
+app.use(express(JSON));
+const courses = [
+    {id: 1, name: 'HTML & CSS'},
+    {id: 2, name: 'JavaScript'},
+    {id: 3, name: 'Node.js'},
+];
 app.get('/', (req, res) => {
     res.send('Hello, World!. meet me');
 })
-app.get('/api/course', (req, res) => {
-    const courses = [
-        {id: 1, name: 'HTML & CSS'},
-        {id: 2, name: 'JavaScript'},
-        {id: 3, name: 'Node.js'},
-    ];
-    res.send(JSON.stringify(courses));
+// app.get('/api/course', (req, res) => {
+//     res.send(JSON.stringify(courses));
+// })
+//route parameters and hadling the GET methods
+app.get('/api/course/:courseID', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.courseID));
+    if (!course) {
+        return res.status(404).send('The course with the given ID was not found.');
+    }
+    else{
+        res.send(course);
+    }
+
+})
+//handling the POST methods
+app.post('/api/course', (req, res) =>{
+    const newCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(newCourse);
+    res.send(newCourse);
 })
 // setting default value of port rather than 3000
 const port = process.env.PORT || 3000;
